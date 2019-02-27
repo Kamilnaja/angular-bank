ae0a1e5f590d68901f8f6e02b56fe4466829d556
najak <kamilnaja@gmail.com>
2019-02-27 23:14:37 +0100

    change redirect

--- src/app/transfer/transfer.routes.module.ts
+++ src/app/transfer/transfer.routes.module.ts
@@ -3,9 +3,10 @@ import { OneTimeTransferComponent } from './one-time-transfer/one-time-transfer.
 import { CyclicTransferComponent } from './cyclic-transfer/cyclic-transfer.component';
 import { CyclicListComponent } from './cyclic-list/cyclic-list.component';
 import { NgModule } from '@angular/core';
+import { TransferComponent } from './transfer.component';
 
 export const transferRoutes: Routes = [
-    { path: '', redirectTo: 'onetimeTransfer', pathMatch: 'full' },
+    { path: '', component: TransferComponent },
     { path: 'onetimeTransfer', component: OneTimeTransferComponent },
     { path: 'cyclicTransfer', component: CyclicTransferComponent },
     { path: 'cyclicListTransfer', component: CyclicListComponent }
