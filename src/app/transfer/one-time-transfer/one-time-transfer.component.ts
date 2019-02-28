e90d692aab2389481fe41dd39f08a440ebbe8612
najak <kamilnaja@gmail.com>
2019-02-28 14:03:54 +0100

    added reactive form

--- src/app/transfer/one-time-transfer/one-time-transfer.component.ts
+++ src/app/transfer/one-time-transfer/one-time-transfer.component.ts
@@ -2,29 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  templateUrl: './one-time-transfer.component.html',
-  styleUrls: ['./one-time-transfer.component.css']
+  templateUrl: "./one-time-transfer.component.html",
+  styleUrls: ["./one-time-transfer.component.css"]
 })
 export class OneTimeTransferComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor(private fb: FormBuilder) { }
-
-  transferGroup = this.fb.group({
-    receiverName: ['']
+  transferForm = this.fb.group({
+    receiverName: [""],
+    fromAccount: [""],
+    toAccount: [""],
+    amount: [""],
+    transferTitle: [""],
+    realizationDate: [""],
+    transferType: [""],
+    additionalOptions: [""]
   });
 
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
-  });
+  ngOnInit() {}
 
-  ngOnInit() {
+  handleSubmit() {
+    console.log(this.transferForm.value);
   }
-
 }
