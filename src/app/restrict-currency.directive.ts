b4a5ee1a8b87f0af8023f8a940009c0bc3e71592
najak <kamilnaja@gmail.com>
2019-03-06 08:13:01 +0100

    added nice directive

---
+++ src/app/restrict-currency.directive.ts
@@ -0,0 +1,25 @@
+import {Directive, ElementRef, HostListener} from '@angular/core';
+
+// https://stackoverflow.com/questions/50722368/limit-input-field-to-two-decimal-places-angular-5
+@Directive({
+  selector: '[appRestrictCurrency]',
+})
+export class RestrictCurrencyDirective {
+  private regex: RegExp = new RegExp(/^\d*\.?\d{0,2}$/g);
+  private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', '-'];
+
+  constructor(private el: ElementRef) {
+  }
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    if (this.specialKeys.indexOf(event.key) !== -1) {
+      return;
+    }
+    const current: string = this.el.nativeElement.value;
+    const next: string = current.concat(event.key);
+    if (next && !String(next).match(this.regex)) {
+      event.preventDefault();
+    }
+  }
+}
