1c7637ee31d863adc9b6722e27f5666c8bed55f1
kamilnaja <kamilnaja@gmail.com>
2019-11-23 20:45:17 +0100

    added unsubscribe

--- src/app/account-form/account-form.component.ts
+++ src/app/account-form/account-form.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { AccountsPayload } from './models/accountsPayload.model';
 import { startWith } from 'rxjs/operators';
+import { AccountsPayload } from './models/accountsPayload.model';
+import { untilDestroyed } from 'ngx-take-until-destroy';
 
 @Component({
   selector: 'app-account-form',
   templateUrl: './account-form.component.html',
   styleUrls: ['./account-form.component.css'],
 })
-export class AccountFormComponent implements OnInit {
-  public formGroup: FormGroup;
-  public allAccounts: AccountsPayload;
-
+export class AccountFormComponent implements OnInit, OnDestroy {
   public get paymentAccount(): FormControl {
     return this.formGroup.get('paymentAccount') as FormControl;
   }
@@ -26,6 +24,12 @@ export class AccountFormComponent implements OnInit {
   }
 
   constructor(private fb: FormBuilder, private route: ActivatedRoute) {}
+  public formGroup: FormGroup;
+  public allAccounts: AccountsPayload;
+
+  ngOnDestroy(): void {
+    console.log('destroying');
+  }
 
   ngOnInit(): void {
     this.buildFormGroup();
@@ -35,16 +39,19 @@ export class AccountFormComponent implements OnInit {
 
   private watchPaymentAccountchanges() {
     this.paymentAccount.valueChanges
-    .pipe(startWith(this.paymentAccount.value))
-    .subscribe((item: Account) => {
-      if (this.repaymentAccount.value === undefined || this.repaymentAccount.value === '') {
-        this.repaymentAccount.setValue(item);
-      }
-    });
+      .pipe(startWith(this.paymentAccount.value), untilDestroyed(this))
+      .subscribe((item: Account) => {
+        if (
+          this.repaymentAccount.value === undefined ||
+          this.repaymentAccount.value === ''
+        ) {
+          this.repaymentAccount.setValue(item);
+        }
+      });
   }
 
   private getInitialData(): void {
-    this.route.data.subscribe(data => {
+    this.route.data.pipe(untilDestroyed(this)).subscribe(data => {
       this.setInitialFormValues(data.accounts.selectedAccount.id);
       this.allAccounts = data.accounts.allAccounts;
     });
